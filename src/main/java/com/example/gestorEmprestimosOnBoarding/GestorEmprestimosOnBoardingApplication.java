c41e7f7e1101bcbd1adb976b5cbb4720024b2f40
João Vitor Tavares <joao.vitor@iwt.com.br>
2026-04-28 09:29:15 -0300

    Projeto Spring Boot criado/Configurado com Docker

---
+++ src/main/java/com/example/gestorEmprestimosOnBoarding/GestorEmprestimosOnBoardingApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestorEmprestimosOnBoarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorEmprestimosOnBoardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorEmprestimosOnBoardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestorEmprestimosOnBoarding/GestorEmprestimosOnBoardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestorEmprestimosOnBoarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorEmprestimosOnBoardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
