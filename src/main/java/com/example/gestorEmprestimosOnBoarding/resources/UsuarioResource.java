2befe97561811e1758b13a09c104cf97a15395c3
João Vitor Tavares <joao.vitor@iwt.com.br>
2026-04-28 10:15:51 -0300

    Testando Primeira Classe de Dominio - Usuario

--- src/main/java/com/example/gestorEmprestimosOnBoarding/resources/UsuarioResource.java
+++ src/main/java/com/example/gestorEmprestimosOnBoarding/resources/UsuarioResource.java
@@ -1,15 +1,27 @@
 package com.example.gestorEmprestimosOnBoarding.resources;
 
+import com.example.gestorEmprestimosOnBoarding.domain.Usuario;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "Minha Rest está funcionando!";
+    public List<Usuario> listar(){
+        Usuario usuario1 = new Usuario(null,"João","joao@gmail.com","TI");
+        Usuario usuario2 = new Usuario(null, "Rafael","rafael@gmail.com","Financeiro");
+
+        List<Usuario> lista = new ArrayList<>();
+        lista.add(usuario1);
+        lista.add(usuario2);
+
+        return lista;
     }
+
 }
